bf17c43a5bb43bbbf4dc82dd3874b724c5393d86
alnahian-hridoy <alnahian.hridoy@connect.qut.edu.au>
2026-06-02 16:27:38 +1000

    .env update DB String

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://alnahian-hridoy:Qwert7244@cluster1.b0t1vmu.mongodb.net/App?appName=Cluster1", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
