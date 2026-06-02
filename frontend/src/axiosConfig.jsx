987a2d0f6f4e821b70d3a118874fdf544e717f34
alnahian-hridoy <alnahian.hridoy@connect.qut.edu.au>
2026-06-03 02:17:25 +1000

    jason package name

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://54.252.251.72:5001', // local
+  baseURL: 'http://54.252.251.72', // local
   //baseURL: 'http://3.26.96.188:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
